3fcf0b58e4f3e4e31c59966aca84324f8156116e
kou164nkn <galahad1005@gmail.com>
2021-08-10 23:41:32 +0900

    added health check function to server

--- go/server/main.go
+++ go/server/main.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/kou164nkn/grpc-sample/go/deepthought"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthgrpc "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/keepalive"
 )
 
@@ -18,6 +20,10 @@ func main() {
 		MinTime: 10 * time.Second,
 	}
 	serv := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(kep))
+
+	hs := health.NewServer()
+	healthgrpc.RegisterHealthServer(serv, hs)
+	hs.Resume()
 
 	deepthought.RegisterComputeServer(serv, &Server{})
 
